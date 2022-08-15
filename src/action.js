557f09a43ee01212ef761b27b5d7a2881a6c1afe
sdaoud <steve@stephendaoud.co.uk>
2022-08-15 11:04:41 +0100

    feat: get sendinblue templates

--- src/action.js
+++ src/action.js
@@ -1,6 +1,23 @@
+const core = require("@actions/core");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const client = SibApiV3Sdk.ApiClient.instance;
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = core.getInput("SENDINBLUE_API_KEY");
+const api = new SibApiV3Sdk.TransactionalEmailsApi();
+
 async function run() {
-  // Test
-  console.log("Hello, world!");
+  const templates = await getTemplates();
+  console.log("TEMPLATES", templates);
+}
+
+async function getTemplates() {
+  const opts = {
+    templateStatus: true,
+    limit: 50,
+    offset: 0,
+  };
+
+  return api.getSmtpTemplates(opts);
 }
 
 run();
